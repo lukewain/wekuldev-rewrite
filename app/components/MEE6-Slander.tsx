cb17918fc17b595537e56ea7f2611385a69fe84d
lukewain <lukewainhouse@outlook.com>
2024-04-13 22:04:45 +0100

    Added new headers and started adding projects

---
+++ app/components/MEE6-Slander.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import React from "react";
+import mee6 from "../../public/MEE6 Slander web.png";
+
+export default function MEE6Slander() {
+  return (
+    <div className="card w-[60%] mx-auto mt-[25px] mb-[20px] bg-[#fff]">
+      <div
+        id="top-bar"
+        className="flex justify-start items-start mt-[4px] ml-[8px]"
+      >
+        <figure>
+          <Image
+            src={mee6}
+            alt="MEE6 Slander Discord Image"
+            width={75}
+            height={75}
+            className="rounded-md mt-[5px]"
+          />
+        </figure>
+        <div className="card-title ml-[9px] mt-[25px]">
+          <h1 className="mx-auto text-center justify-center underline underline-offset-4 text-[#000000]">
+            MEE6 Slander
+          </h1>
+        </div>
+      </div>
+      <div className="card-body">
+        <div className="w-[95%] mx-auto">
+          <p>
+            MEE6 Slander originally started out as a funny bot for me and my
+            friends to laugh about how MEE6 was annoying and always messaged at
+            the most inconvenient of times.
+            <br />
+            <br />
+            Since its creation, the bot has grown to exist in over 1,000 servers
+            and is used by over 20,000 people. If you'd like you can check out
+            the source code or even invite the bot to your own server. Just make
+            sure you have MEE6 there otherwise you won't benefit!
+          </p>
+        </div>
+        <div className="card-actions justify-center mt-[10px]">
+          <a href="https://github.com/lukewain/MEE6-Slander">
+            <button className="btn btn-primary">View the source</button>
+          </a>
+          <a href="https://discord.com/oauth2/authorize?client_id=1013859380652953671&permissions=275414845440&scope=applications.commands+bot">
+            <button className="btn btn-primary">Add the bot</button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import MEE6Slander from "./components/MEE6-Slander";
 
 export default function Page() {
   return (
@@ -7,63 +8,76 @@ export default function Page() {
         <p className="text-[#ffff] text-6xl font-mono text-center">wekul.dev</p>
         <br />
       </div>
-      <div
-        className="flex flex-col lg:flex-row place-content-center justify-center items-center"
-        id="fade-in"
-      >
-        <div className="lg:mr-[20px] mb-[5px] lg:mb-[0px]" id="card1">
-          <div className="card shadow-xl w-96 h-[439px] bg-[#ffff] text-[#000000]">
-            <figure>
-              <img
-                width="50%"
-                height="50%"
-                src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"
-                alt="Github Logo"
-              />
-            </figure>
-            <div className="card-title flex flex-col mt-[5px]">
-              <p className="text-md">GitHub</p>
-            </div>
-            <div className="card-body items-center">
-              The GitHub where all my projects can be found!
-            </div>
-            <div className="card-actions justify-center mb-[18px]">
-              <a
-                href="https://github.com/lukewain"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <button className="btn btn-primary text-[#ffff]">
-                  My GitHub!
-                </button>
-              </a>
+      <hr className="flex m-auto sm:w-[95%] lg:w-[98%] h-2 justify-center items-center place-content-center" />
+      <div id="project-list">
+        {/* All the projects ive done and what about */}
+        <h1 className="font-mono text-4xl ml-10 mt-5 underline underline-offset-2">
+          My Projects
+        </h1>
+        <MEE6Slander />
+      </div>
+      <div id="socials-list" className="mt-[90px] mb-[20px]">
+        <h1 className="font-mono text-4xl ml-10 mt-5 underline underline-offset-2">
+          My Connections
+        </h1>
+        <div
+          className="flex flex-col lg:flex-row place-content-center justify-center items-center mt-[20px]"
+          id="fade-in"
+        >
+          <div className="lg:mr-[20px] mb-[5px] lg:mb-[0px]" id="card1">
+            <div className="card shadow-xl w-[300px] h-[309px] bg-[#ffff] text-[#000000]">
+              <figure>
+                <img
+                  width="25%"
+                  height="25%"
+                  src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"
+                  alt="Github Logo"
+                />
+              </figure>
+              <div className="card-title flex flex-col mt-[5px]">
+                <p className="text-md">GitHub</p>
+              </div>
+              <div className="card-body items-center">
+                The GitHub where all my projects can be found!
+              </div>
+              <div className="card-actions justify-center mb-[18px]">
+                <a
+                  href="https://github.com/lukewain"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <button className="btn btn-primary text-[#ffff]">
+                    My GitHub!
+                  </button>
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="lg:ml-[20px] mt-[5px] lg:mt-[0px]" id="card2">
-          <div className="card shadow-xl w-96 h-[439px] bg-[#ffff] text-[#000000]">
-            <figure>
-              <img
-                width="50%"
-                height="50%"
-                src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/252250107793711.5faee56d2d7de.png"
-                alt="Discord Logo"
-              />
-            </figure>
-            <div className="card-title flex flex-col mt-[5px]">
-              <p className="text-md">Discord</p>
-            </div>
-            <div className="card-body items-center">My discord profile!</div>
-            <div className="card-actions justify-center mb-[18px]">
-              <a
-                href="https://discordapp.com/users/268815279570681857"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <button className="btn btn-primary text-[#ffff]">
-                  Add Me!
-                </button>
-              </a>
+          <div className="lg:ml-[20px] mt-[5px] lg:mt-[0px]" id="card2">
+            <div className="card shadow-xl w-[300px] h-[309px] bg-[#ffff] text-[#000000]">
+              <figure>
+                <img
+                  width="25%"
+                  height="25%"
+                  src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/252250107793711.5faee56d2d7de.png"
+                  alt="Discord Logo"
+                />
+              </figure>
+              <div className="card-title flex flex-col mt-[5px]">
+                <p className="text-md">Discord</p>
+              </div>
+              <div className="card-body items-center">My discord profile!</div>
+              <div className="card-actions justify-center mb-[18px]">
+                <a
+                  href="https://discordapp.com/users/268815279570681857"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <button className="btn btn-primary text-[#ffff]">
+                    Add Me!
+                  </button>
+                </a>
+              </div>
             </div>
           </div>
         </div>
